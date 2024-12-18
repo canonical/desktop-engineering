88b2c2e77f5b850cbca6df9c88a881e034c025a5
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2024-12-18 10:53:29 +0100

    Use `find` instead of hardcoding the paths

--- gh-actions/C/format-source.sh
+++ gh-actions/C/format-source.sh
@@ -1,38 +1,52 @@
 #!/usr/bin/env bash
 
-# Configure here all the directories where are source files
-SOURCE_FILES="src/*.[ch] tests/*.[ch]"
-
-if [[ "$1" == "pre-commit" ]]; then
-    echo Checking source style
-    PRE_COMMIT=1
+if [ $# -eq 0 ]; then
+    FORMAT_CODE=1
 else
-    PRE_COMMIT=0
+    FORMAT_CODE=0
+    if [[ "$1" == "pre-commit" ]]; then
+        echo Checking source style
+        PRE_COMMIT=1
+    else
+        PRE_COMMIT=0
+    fi
 fi
 
-passed=true
-for file in $SOURCE_FILES; do
-    if [ $# -eq 0 ]; then
+check_source_file() {
+    if [ $FORMAT_CODE -eq 1 ]; then
         # no parameters? Just apply the changes
-        echo Formating "$file"
-        clang-format -i "$file"
+        echo Formating "$1" >&2
+        clang-format -i "$1" >&2
     else
         # any parameter? check that the formatting is fine
-        clang-format "$file" > "$file.formatted"
-        echo Checking $file
+        clang-format "$1" > "$1.formatted"
+        echo Checking $1 >&2
         if [ $PRE_COMMIT -eq 0 ]; then
-            if ! diff "$file" "$file.formatted"; then
-                passed=false
+            if ! diff "$1" "$1.formatted" >&2; then
+                echo false
             fi
         else
-            if ! diff "$file" "$file.formatted" > /dev/null; then
-                passed=false
+            if ! diff "$1" "$1.formatted" > /dev/null; then
+                echo false
             fi
         fi
-        rm "$file.formatted"
+        rm "$1.formatted"
     fi
-done
-if [ $passed = false ]; then
+}
+
+# a trick to ensure that names/folders with blank spaces do work as expected
+# also, since find and while read... are executed in a subshell, they can't
+# modify any variable outside. That's why we capture the output in $RESULT
+# and compare it with an empty string.
+# Of course, for this to work, it is mandatory to send any visible message to
+# STDERR. That's why the check_source_file() function has so many >&2: those
+# are commands whose output we want in the screen. This trick is needed because
+# there seems to not be an easy way of storing STDERR instead of SDTOUT
+RESULT=$(find -name '*.c' -or -name '*.h' | while read file; do
+    check_source_file "$file"
+done)
+
+if [ ! $RESULT = "" ]; then
     echo Failed to pass clang-format check
     exit 1
 fi
